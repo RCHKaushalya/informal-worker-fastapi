0eaf88dcbebe9bfe2b7f78d90f96e65ea657d241
RCHKaushalya <rchkaushalya@gmail.com>
2026-03-19 21:29:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from routes import users, jobs, skills
 
 app = FastAPI()
 
+@app.post('')
+def home():
+    return {'message': 'Informal worker platform'}
+
 app.include_router(users.router)
 app.include_router(jobs.router)
 app.include_router(skills.router)
